b87988ec6487c3d301d3ed0dd2552af02f84b1d3
PavelSH <serfer94@gmail.com>
2023-12-28 16:44:47 +0100

    task-01 optimization

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# devops-hometasks
+# Homework for Continuous Integration and Continuous Deployment (DevOps) course at the IT-Academy (Minsk, Belarus).
+# Started 06.2022, finished 10.2022.   
+
+- [Homework 1](02-tools/README.md#homework-1) (git,vagrnat)
+- [Homework 2](03-lxc/README.md#homework-2) (lxc)
+- [Homework 3](04-5-ansible/README.md#homework-3) (ansible)
+- [Homework 4](06-puppet/README.md#homework-4) (puppet)
+- [Homework 5](07-puppet/README.md#homework-5) (puppet)
+- [Homework 6](08-jenkins/README.md#homework-6) (jenkins)
+- [Homework 7](09-nexus/README.md#homework-7) (nexus)
+- [Homework 8](10-pipeline/README.md#homework-8) (Jenkinsfile)
+- [Homework 9](11-docker/README.md#homework-9) (Dockerfile)
+- [Homework 10](12-docker-compose/README.md#homework-10) (docker-compose)
+- [Homework 11](14-github-actions/README.md#homework-11) (github-actions)
+- [Homework 12](16-17-kubernetes/README.md#homework-12) (kubernetes)
+- [Homework 13](18-19-terraform/README.md#homework-13) (terraform)
+- [Homework final](13-jenkins-docker/README.md#homework-final) (final)
--- task-01/provision.sh
+++ task-01/provision.sh
@@ -1,28 +1,33 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install -y apache2 php
+#!/bin/bash
 
-php -v
+# Update and install packages
+sudo apt-get update && \
+sudo apt-get install -y --no-install-recommends \
+apache2 \
+php && \
+sudo apt-get autoremove -y && \
+sudo apt-get clean && \
+sudo rm -rf /var/lib/apt/lists/*
 
-echo 'Listen 81' >> /etc/apache2/ports.conf
+# Expose port for php
+echo 'Listen 81' | sudo tee -a /etc/apache2/ports.conf
 
-#create dir siteone
-mkdir -p /var/www/siteone/html
-cp /home/vagrant/index/index.html /var/www/siteone/html
-cp /home/vagrant/conf/siteone.conf /etc/apache2/sites-available/
+# Prepare files for static page
+mkdir -p /var/www/static_page
+cp /home/vagrant/index/index.html /var/www/static_page
+cp /home/vagrant/conf/static_page.conf /etc/apache2/sites-available/
 
-#create dir sitetwo
-mkdir -p /var/www/sitetwo/html
-cp /home/vagrant/index/index.php /var/www/sitetwo/html
-cp /home/vagrant/conf/sitetwo.conf /etc/apache2/sites-available/
+# Prepare files for dynamic page
+mkdir -p /var/www/dynamic_page
+cp /home/vagrant/index/index.php /var/www/dynamic_page
+cp /home/vagrant/conf/dynamic_page.conf /etc/apache2/sites-available/
 
-#active siteone and sitetwo
-sudo a2ensite siteone.conf
-sudo a2ensite sitetwo.conf
+# Activate sites
+sudo a2ensite static_page dynamic_page
 sudo a2dissite 000-default.conf
 
-#start php7.4
+# Enable PHP module
 sudo a2enmod php7.*
 
-#restart apache
+# Restart apache
 systemctl restart apache2
