d8c3ba5a20d79a3b6e499f74b8629252c2a3e68c
PavelSH <serfer94@gmail.com>
2023-12-28 17:16:12 +0100

    Replace index.php

--- task-01/index/index.php
+++ task-01/index/index.php
@@ -1,8 +1,13 @@
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <p>Your Vagrantfile is fine if you can see this message.</p>
- </body>
+  <head>
+    <title>Site is running PHP version <?= phpversion(); ?></title>
+  </head>
+  <body>
+    <?php
+      $limit = rand(1, 1000);
+      for ($i=0; $i<$limit; $i++){
+        echo "<p>Hello, world!</p>";
+      }
+    ?>
+  </body>
 </html>
